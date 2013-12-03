9b4bec3c598d72fd6edefce79353c8fb5bf91cdd
yifei <flylee.bjfu@gmail.com>
2013-12-04 00:59:28 +0800

    添加admin模板

--- controller/admin.class.php
+++ controller/admin.class.php
@@ -9,10 +9,24 @@ class adminController extends coreController
 		// 载入默认的
 		parent::__construct();
 	}
-
+	// 首页
 	function index()
 	{
-		
+		$data['title'] = 'RedBeanPie管理后台';
+		render($data, 'admin', 'index');
+	}
+	
+	// 用户管理，预注册
+	function preuser() {
+		render($data, 'admin', 'index');
+	}
+	// 公司管理，公司信息
+	function cominfo() {
+		render($data, 'admin', 'index');
+	}
+	// 公司管理，行业信息
+	function industryinfo() {
+		render($data, 'admin', 'index');
 	}
 	
 	
