106c90dabc28ed6870d397eda1ebd64f76cb3ccb
yifei <flylee.bjfu@gmail.com>
2014-01-21 11:05:09 +0800

    样式，导航发起按钮

--- static/dev/js/mods/common.js
+++ static/dev/js/mods/common.js
@@ -4,17 +4,24 @@ var siteDomain = 'http://dev.redbeanpie.com/';
 var launchTPL = [
 	'<div class="list-group list-group-launch text-center">',
 		'<a href="/?c=party&a=launch" class="list-group-item">',
-			'派对',
+			'<span class="list-group-icon"><img src="/static/dev/images/icon-launch-party.png"></span>',
+			'<span>派对</span>',
 		'</a>',
 		'<a href="/?c=dating&a=launch" class="list-group-item">',
-			'约会',
+			'<span class="list-group-icon"><img src="/static/dev/images/icon-launch-dating.png"></span>',
+			'<span>约会</span>',
 		'</a>',
 	'</div>'
 ];
+
 // 发起按钮
-$('#launch-btn').popover({
-	container : 'body',
-	html : true,
-	placement : 'bottom',
-	content : launchTPL.join('')
-});
+if($('#launch-btn').length) {
+	$('#launch-btn').popover({
+		container : 'body',
+		html : true,
+		placement : 'bottom',
+		animation : false,
+		trigger : 'click',
+		content : launchTPL.join('')
+	});
+}
