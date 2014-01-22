3fe6c79e8ab50dce977d8415b982063929716b2a
flylee2011 <flylee.bjfu@gmail.com>
2014-01-22 23:51:17 +0800

    party dating page

--- static/dev/js/mods/common.js
+++ static/dev/js/mods/common.js
@@ -3,13 +3,13 @@ var siteDomain = 'http://dev.redbeanpie.com/';
 // 发起浮层模板
 var launchTPL = [
 	'<div class="list-group list-group-launch text-center">',
-		'<a href="/?c=party&a=launch" class="list-group-item">',
-			'<span class="list-group-icon"><img src="/static/dev/images/icon-launch-party.png"></span>',
-			'<span>派对</span>',
-		'</a>',
 		'<a href="/?c=dating&a=launch" class="list-group-item">',
 			'<span class="list-group-icon"><img src="/static/dev/images/icon-launch-dating.png"></span>',
 			'<span>约会</span>',
+		'</a>',
+		'<a href="/?c=party&a=launch" class="list-group-item">',
+			'<span class="list-group-icon"><img src="/static/dev/images/icon-launch-party.png"></span>',
+			'<span>派对</span>',
 		'</a>',
 	'</div>'
 ];
